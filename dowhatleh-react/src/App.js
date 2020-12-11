418e77a7f58e1f0e4f48db152e7051733dcb9eab
ricebun <renicegohyc@gmail.com>
2020-12-11 18:06:04 +0800

    added basic page structure

--- dowhatleh-react/src/App.js
+++ dowhatleh-react/src/App.js
@@ -1,8 +1,25 @@
+import { Grommet, Box } from "grommet";
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
 function App() {
   return (
-    <div className="App">
-      <p>Don't Know</p>
-    </div>
+    <Grommet theme={theme} full>
+      <Box fill>
+        <Box flex>Home</Box>
+        <Box flex>Input</Box>
+        <Box flex>Result</Box>
+        <Box flex>Selection Details</Box>
+      </Box>
+    </Grommet>
   );
 }
 
